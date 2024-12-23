096418da56ba6669b050cc4bfa192d549d713be2
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2024-12-23 20:14:51 +0300

    clear fields

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function calculateSum() {
+
+    // Get values
     const num1 = parseFloat(document.getElementById('num1').value) || 0;
     const num2 = parseFloat(document.getElementById('num2').value) || 0;
+
+
+    // Calc or sum
     const sum = num1 + num2;
 
+    // show up the sum
     document.getElementById('result').textContent = `Result: ${sum}`;
+
+
+    // Clear the input fields
+    document.getElementById('num1').value = '';
+    document.getElementById('num2').value = '';
 }
