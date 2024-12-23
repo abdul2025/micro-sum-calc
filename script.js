18b45dea83fa9cf714deb4f4e92906c7f7d901f5
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2024-12-23 20:09:17 +0300

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculateSum() {
+    const num1 = parseFloat(document.getElementById('num1').value) || 0;
+    const num2 = parseFloat(document.getElementById('num2').value) || 0;
+    const sum = num1 + num2;
+
+    document.getElementById('result').textContent = `Result: ${sum}`;
+}
